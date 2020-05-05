72cd8a5785a7f7c2b749caa3b6a3c1a2cd15816a
Tiago Roberto <tiago.parra.dev@outlook.com>
2020-05-05 15:00:52 -0300

    Segundo Commit

---
+++ exercicicoSemana01_PBD_EX_03.py
@@ -0,0 +1,6 @@
+# This Python file uses the following encoding: utf-8
+import os, sys, math
+
+raio = float(input('Digite o raio do circulo: '))
+area = math.pi * raio**2
+print ('A área do círculo é ' + str(area))
---
+++ exercicicoSemana01_PBD_EX_04.py
@@ -0,0 +1,11 @@
+# This Python file uses the following encoding: utf-8
+import os, sys, math
+
+pes = float(input('Digite a medida em pés: '))
+polegadas = pes * 12
+jardas = pes / 3
+milhas = jardas / 1760
+print(str(pes) + " pes equivalem a ")
+print(str(polegadas)+ " polegadas")
+print(str(jardas) + " jardas e ")
+print(str(milhas) + " milhas")
